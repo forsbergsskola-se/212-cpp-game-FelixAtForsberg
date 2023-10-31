c29c3a5fa938b8614e4b679b1aaf33a1fd231d19
FelixAtForsberg <felix.berge@student.forsbergsskola.se>
2023-10-31 23:08:21 +0100

    Debug prints (is a header only, for now)

---
+++ source/system/debug_prints.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#define CON_ESC         0x1B
+#define CON_SEP         0x3B
+#define CON_STYLE_RESET 0x6D
+#define CON_COL_FG_RED  0x33316D
+
+#include <iostream>
+class DebugLog {
+public:
+
+    template <typename... TPrintArgs>
+    static void LogFailure(TPrintArgs... printArgs);
+};
+
+template<typename... TPrint>
+void DebugLog::LogFailure(TPrint... printArgs) {
+
+    for (auto const& printArg : {printArgs...}) {
+        std::cout << printArg;
+        std::cout << ' ' << std::flush;
+    }
+    std::cout << '\n' << std::flush; // on second thought,
+                                    // this clang-tidy rule might be
+                                    // completely useless
+}
